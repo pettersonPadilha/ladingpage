aba23cded846c56b25d9f86a19464d34110d5a75
Petter Oliveira <petterson_sorriso@hotmail.com>
2022-10-23 03:26:14 -0400

    Testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { feedback } from "../constants";
+import styles from "../style";
+import FeedbackCard from "./FeedbackCard";
 
-const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
-}
+const Testimonials = () => (
+  <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
 
-export default Testimonials
+    
+  </section>
+);
+
+export default Testimonials;
