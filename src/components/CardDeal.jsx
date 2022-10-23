2225b3614e6f1ba84918b21c1f6ffd7f3d63188d
Petter Oliveira <petterson_sorriso@hotmail.com>
2022-10-23 03:24:42 -0400

    carDeal

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -9,7 +9,13 @@ const CardDeal = () => (
         Find a better card deal <br className="sm:block hidden" /> in few easy
         steps.
       </h2>
-     
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+        aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
 
     <div className={layout.sectionImg}>
       <img src={card} alt="billing" className="w-[100%] h-[100%]" />
