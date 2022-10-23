9d213c41184093e29c8312e99bdd17a612b957ce
Petter Oliveira <petterson_sorriso@hotmail.com>
2022-10-23 03:13:44 -0400

    stats hero

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
-const Stats = () => {
-  return (
-    <div>Stats</div>
+import {stats} from '../constants';
+import styles from '../style'
+const Stats = () =>  (
+    <section className={`${styles.flexCenter}
+    flex-row flex-wrap sm:mb-20px mb-6`}>
+      {stats.map((stat) =>(
+        <div
+        key={stat.key}
+        className={`flex-1 flex justify-start items-center flex-row m-3`}
+        >
+          <h4 className="font-roboto 
+          font-semibold xs:text-[40px] 
+          text-[30px] xs:leading-[53px] 
+          leading-[43px] text-white">
+            {stat.value}</h4>
+          <p
+          className="font-roboto font-normal 
+          xs:text-[20px] text-[15px]
+          xs:leading-[26px] 
+          leading-[21px] text-gradient uppercase ml-3"
+          >{stat.title}</p>
+        </div>
+      ))}
+    </section>
   )
-}
+
 
 export default Stats
